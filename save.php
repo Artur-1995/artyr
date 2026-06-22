b5a97f9007e1f499e554e3e109928ecb3fd40cd2
Artur-1995 <89254423508@mail.ru>
2026-06-22 08:26:06 +0300

    Финальные доработки к первому заданию
    Доработки:
    
    Добавить дату
    
    Редактирование проектов
    
    Адаптировать под мобильную версию
    
    На мобильных телефонах (<576px): 1 карточка в ряд
    
    На планшетах (576-768px): 2 карточки в ряд
    
    На больших экранах (>768px): 3 карточки в ряд

--- save.php
+++ save.php
@@ -5,9 +5,22 @@ $link = $_POST['link'] ?? '';
 $image = $_POST['image'] ?? '';
 $date = $_POST['date'] ?? '';
 
+$data = json_decode(file_get_contents('projects.json'), true) ?? [];
+$project_data = [];
+
+
+foreach ($data as $key => $project) {
+    if ($project['title'] === $title) {
+        $data[$title] = ['title' => $title, 'description' => $desc, 'link' => $link, 'image' => $image, 'date' => $date];
+        file_put_contents('projects.json', json_encode($data, JSON_PRETTY_PRINT));
+
+        header('Location: index.php');
+        exit;
+    }
+}
+
 if ($title && $desc) {
-    $data = json_decode(file_get_contents('projects.json'), true) ?? [];
-    $data[] = ['title' => $title, 'description' => $desc, 'link' => $link, 'image' => $image, 'date' => $date];
+    $data[$title] = ['title' => $title, 'description' => $desc, 'link' => $link, 'image' => $image, 'date' => $date];
     touch($title . '.php');
     file_put_contents('projects.json', json_encode($data, JSON_PRETTY_PRINT));
 }
